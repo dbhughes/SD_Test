ff6656deab926c32dc5e2fd16ae1bc96727e5d72
Derek Hughes <dbh@ccomps.com>
2024-02-01 12:44:23 -0600

    Initial changes to copied project

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,3 @@
-/*
- * pin 1 - not used          |  Micro SD card     |
- * pin 2 - CS (SS)           |                   /
- * pin 3 - DI (MOSI)         |                  |__
- * pin 4 - VDD (3.3V)        |                    |
- * pin 5 - SCK (SCLK)        | 8 7 6 5 4 3 2 1   /
- * pin 6 - VSS (GND)         | ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄  /
- * pin 7 - DO (MISO)         | ▀ ▀ █ ▀ █ ▀ ▀ ▀ |
- * pin 8 - not used          |_________________|
- *                             ║ ║ ║ ║ ║ ║ ║ ║
- *                     ╔═══════╝ ║ ║ ║ ║ ║ ║ ╚═════════╗
- *                     ║         ║ ║ ║ ║ ║ ╚══════╗    ║
- *                     ║   ╔═════╝ ║ ║ ║ ╚═════╗  ║    ║
- * Connections for     ║   ║   ╔═══╩═║═║═══╗   ║  ║    ║
- * full-sized          ║   ║   ║   ╔═╝ ║   ║   ║  ║    ║
- * SD card             ║   ║   ║   ║   ║   ║   ║  ║    ║
- * Pin name         |  -  DO  VSS SCK VDD VSS DI CS    -  |
- * SD pin number    |  8   7   6   5   4   3   2   1   9 /
- *                  |                                  █/
- *                  |__▍___▊___█___█___█___█___█___█___/
- *
- * Note:  The SPI pins can be manually configured by using `SPI.begin(sck, miso, mosi, cs).`
- *        Alternatively, you can change the CS pin and use the other default settings by using `SD.begin(cs)`.
- *
- * +--------------+---------+-------+----------+----------+----------+
- * | SPI Pin Name | ESP8266 | ESP32 | ESP32-S2 | ESP32-C3 | ESP32-S3 |
- * +==============+=========+=======+==========+==========+==========+
- * | CS (SS)      | GPIO15  | GPIO5 | GPIO5    | GPIO13   | GPIO13   |
- * +--------------+---------+-------+----------+----------+----------+
- * | DI (MOSI)    | GPIO13  | GPIO23| GPIO24   | GPIO14   | GPIO14   |
- * +--------------+---------+-------+----------+----------+----------+
- * | DO (MISO)    | GPIO12  | GPIO19| GPIO25   | GPIO15   | GPIO15   |
- * +--------------+---------+-------+----------+----------+----------+
- * | SCK (SCLK)   | GPIO14  | GPIO18| GPIO19   | GPIO16   | GPIO16   |
- * +--------------+---------+-------+----------+----------+----------+
- *
- * For more info see file README.md in this library or on URL:
- * https://github.com/espressif/arduino-esp32/tree/master/libraries/SD
- */
 
 #include "FS.h"
 #include "SD.h"
@@ -51,7 +12,7 @@ char txtBuf[250];
 
 // Assign names to Pushbutton pins
 const int PushButton0 = 36;
-
+char* logFileName="WLog.txt";
 
 void setup()
 {
@@ -68,33 +29,25 @@ void setup()
 /*
     mysd.listDir(SD, "/", 0);
     mysd.createDir(SD, "/mydir");
-    mysd.listDir(SD, "/", 0);
     mysd.removeDir(SD, "/mydir");
-    mysd.listDir(SD, "/", 2);
     mysd.writeFile(SD, "/hello.txt", "Hello ");
     mysd.appendFile(SD, "/hello.txt", "World!\n");
     mysd.readFile(SD, "/hello.txt");
     mysd.deleteFile(SD, "/foo.txt");
     mysd.renameFile(SD, "/hello.txt", "/foo.txt");
     mysd.readFile(SD, "/foo.txt");
-    mysd.testFileIO(SD, "/test.txt");
-
-    mysd.writeFile(SD, "/Derek.txt", "Hello Derek");
-    mysd.writeFile(SD, "/BILLY.txt", "Hello BB");
 */
 
-    mysd.fileExists(SD, "/Frogger.txt");
+    
 
-    if (!mysd.fileExists(SD, "/Logger.txt"))
+    if (!mysd.fileExists(SD, logFileName))
     {
-        mysd.writeFile(SD, "/Logger.txt", "Hello Loggy\n");
-        mysd.appendFile(SD, "/Logger.txt",  " What\n");
-        mysd.appendFile(SD, "/Logger.txt",  " a\n");
-        mysd.appendFile(SD, "/Logger.txt",  " Great\n");
-        mysd.appendFile(SD, "/Logger.txt",  " World!\n");
+        mysd.writeFile(SD, logFileName, "Hello Loggy\n");
     }
     
-    mysd.readFile(SD, "/Logger.txt");
+    mysd.appendFile(SD, logFileName,  " World!\n");
+
+    mysd.readFile(SD, logFileName);
 
 
 
